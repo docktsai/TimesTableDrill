be2e57f206d27509f28b06dc8c667e3236d67075
Dock Tsai <docktsai@gmail.com>
2017-08-20 07:55:55 -0400

    change app launcher icon

--- app/src/main/java/com/dockdesignstudio/timestabledrill/DrillActivity.java
+++ app/src/main/java/com/dockdesignstudio/timestabledrill/DrillActivity.java
@@ -1,17 +1,38 @@
 package com.dockdesignstudio.timestabledrill;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class DrillActivity extends AppCompatActivity {
+
+    Integer number1=4;
+    Integer number2=6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drill);
-        String question = "5X7 = ";
-        TextView textView = (TextView) findViewById(R.id.textView3);
-        textView.setText(question);
+
+        String question = number1.toString() + "X"+number2.toString()+ " = ";
+        TextView tvQuestion = (TextView) findViewById(R.id.tvQuestion);
+        tvQuestion.setText(question);
+    }
+    public void checkAnswer(View view){
+        EditText etAnswer = (EditText) findViewById(R.id.etAnswer);
+        TextView tvAnswer = (TextView) findViewById(R.id.tvAnswer);
+        TextView tvChkResult = (TextView) findViewById(R.id.tvChkResult);
+
+        String strAnswer = etAnswer.getText().toString();
+        Integer answer = Integer.parseInt(strAnswer);
+        tvAnswer.setText(strAnswer);
+
+        if (answer == (number1*number2)){
+            tvChkResult.setText("Correct!");
+        } else {
+            tvChkResult.setText("Wrong!");
+        }
     }
 }
--- app/src/main/java/com/dockdesignstudio/timestabledrill/MainActivity.java
+++ app/src/main/java/com/dockdesignstudio/timestabledrill/MainActivity.java
@@ -10,10 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Integer number1=4;
-        Integer number2=6;
-        String question = number1.toString() + "X"+number2.toString();
-
         setContentView(R.layout.activity_main);
     }
 
